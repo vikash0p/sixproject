175a43d7c31b632afa468b95dbaf5ab7f97cd5fd
Vikash <vikash2000158@gmail.com>
2023-05-18 16:39:39 +0530

    heading wrire

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ const Home = async() => {
   const product = await res.json();
   return (
     <div>
-      
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo earum illo provident, natus numquam atque cupiditate fuga, iste repudiandae impedit accusamus sapiente, sed ullam maiores. Veritatis obcaecati beatae temporibus esse vitae culpa nam cupiditate, ab aliquid, velit quia nobis exercitationem corrupti consequatur labore. Quo blanditiis nisi itaque ratione ipsa rem?</h1>
     </div>
   )
 }
